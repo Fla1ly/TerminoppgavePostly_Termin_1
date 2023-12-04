6a912ceb081b752a61619d46c74493f5bce16b0b
Dawid <musiald20@gmail.com>
2023-12-04 11:56:10 +0100

    commiting changes

--- components/connect.php
+++ components/connect.php
@@ -1,9 +1,17 @@
 <?php
 
-   $db_name = 'mysql:host=localhost;dbname=blog_db';
-   $user_name = 'root';
-   $user_password = 'Admin';
+$db_host = 'localhost';
+$db_name = 'blog_db';
+$db_user = 'Dawid';
+$db_password = 'Admin';
 
-   $conn = new PDO($db_name, $user_name, $user_password);
+try {
+    $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
 
 ?>
